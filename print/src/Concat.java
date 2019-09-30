9afaf7c226245fba5b62a481cbadfd2adde39864
Samolberg <l.samolberg@khk.ee>
2019-09-30 14:38:20 +0300

    Calculations

---
+++ print/src/Concat.java
@@ -0,0 +1,15 @@
+public class Concat {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 4;
+        int num3 = num1 + num2;
+        int num4 = num1 * num2;
+
+        System.out.println(num1 + " + " + num2 + " = " + num3 );
+        System.out.println(num1 + " * " + num2 + " = " + num4);
+
+
+
+
+    }
+}
