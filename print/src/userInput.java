dd69ba043ae3585f011fdd97dc21a969b6d906ce
Samolberg <l.samolberg@khk.ee>
2019-09-30 15:15:29 +0300

    Calculations 12

--- print/src/userInput.java
+++ print/src/userInput.java
@@ -4,17 +4,22 @@ import java.util.Scanner;
 public class userInput {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int num1 = Integer.parseInt(reader.nextLine());
-        System.out.print("Enter another number: ");
-        int num2 = Integer.parseInt(reader.nextLine());
-        int num3 = num1 * num2;
-        System.out.println("Calculation with numbers: " + num3);
+        System.out.println("Your name: ");
+        String name1 = reader.nextLine();
 
-        System.out.println("Enter radius ");
-        int radius = Integer.parseInt(reader.nextLine());
-        double circumference = 2 * Math.PI * radius;
-        System.out.println("Circumference of the circle :" + circumference);5
+        System.out.print("Enter age: ");
+        int num1 = Integer.parseInt(reader.nextLine());
+
+        System.out.println("Enter another name: ");
+        String name2 = reader.nextLine();
+
+        System.out.print("Enter age: ");
+        int num2 = Integer.parseInt(reader.nextLine());
+
+        int num3 = num1 + num2;
+        System.out.println(name1 + " and "+ name2 + " are " + num3 + " years old in total");
+
+
 
     }
 }
