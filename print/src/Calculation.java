f679906d758ff532e41a9e0e08fac3447a7aa28d
Samolberg <l.samolberg@khk.ee>
2019-09-30 14:29:53 +0300

    Calculations

--- print/src/Calculation.java
+++ print/src/Calculation.java
@@ -2,7 +2,11 @@ public class Calculation {
     public static void main(String[] args) {
         int days = 365;
         int hours = 24;
-        int seconds = 60;
+        int minutes = 60;
+        int seconds = days * hours * minutes * 60;
+
+        System.out.println(seconds);
+
 
 
     }
