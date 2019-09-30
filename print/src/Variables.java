d578a8aab30577fa026558c140ed58dd6a2ddaff
Samolberg <l.samolberg@khk.ee>
2019-09-30 11:09:34 +0300

    Test commit

---
+++ print/src/Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String[] args) {
+        int chickens = 9000;
+        double bacon = 0.1;
+        String tractor = "Zetor";
+
+        System.out.println("Chickens: \n" + chickens);
+        System.out.println("Backon (kg): \n" + bacon);
+        System.out.println("A tractor: \n" + tractor);
+    }
+}
