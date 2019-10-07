6b27c61e6c6477e9ae7d06f89a5989b915ee970f
Samolberg <l.samolberg@khk.ee>
2019-10-07 11:19:33 +0300

    Conditional statement 16

--- print/src/condStatement.java
+++ print/src/condStatement.java
@@ -3,12 +3,13 @@ public class condStatement {
 
         public static void main(String[] args) {
             Scanner reader = new Scanner(System.in);
-            System.out.print("How old are you: ");
+            System.out.print("Type a number: ");
             int num1 = Integer.parseInt(reader.nextLine());
-            if (num1 >= 18) {
-                System.out.println("You have reached the age of majority");
+
+            if (num1%2 == 0) {
+                System.out.println("Even number");
             } else {
-                System.out.println("You have not reached the age");
+                System.out.println("Not even");
             }
         }
     }
