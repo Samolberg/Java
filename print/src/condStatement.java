f0886de2d224ff4c1c4a9b4a6af330b99840d6d1
Samolberg <l.samolberg@khk.ee>
2019-10-07 11:59:19 +0300

    Conditional statement 18 - grades

--- print/src/condStatement.java
+++ print/src/condStatement.java
@@ -3,18 +3,21 @@ public class condStatement {
 
         public static void main(String[] args) {
             Scanner reader = new Scanner(System.in);
-            System.out.print("Type a number: ");
+            System.out.print("Type a number: 0-60 ");
             int num1 = Integer.parseInt(reader.nextLine());
 
-            System.out.print("Type a number: ");
-            int num2 = Integer.parseInt(reader.nextLine());
-
-            if (num1 > num2) {
-                System.out.println("Bigger number: " + num1);
-            } else if (num2 > num1) {
-                System.out.println("Bigger number:" + num2);
+            if (num1 >= 50 ) {
+                System.out.println("Grade: 5");
+            } else if (num1 >= 45) {
+                System.out.println("Grade: 4");
+            }else if (num1 >= 40) {
+                System.out.println("Grade: 3");
+            }else if (num1 >= 35) {
+                System.out.println("Grade: 2");
+            }else if (num1 >= 30) {
+                System.out.println("Grade: 1");
             } else {
-                System.out.println("Numbers are equal");
+                System.out.println("Failed");
             }
         }
     }
