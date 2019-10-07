27327e991a153e92b470ad21c1a2adbe829d97b6
Samolberg <l.samolberg@khk.ee>
2019-10-07 12:06:56 +0300

    Conditional statement 19 - age check

--- print/src/condStatement.java
+++ print/src/condStatement.java
@@ -3,21 +3,13 @@ public class condStatement {
 
         public static void main(String[] args) {
             Scanner reader = new Scanner(System.in);
-            System.out.print("Type a number: 0-60 ");
+            System.out.print("Type in your age ");
             int num1 = Integer.parseInt(reader.nextLine());
 
-            if (num1 >= 50 ) {
-                System.out.println("Grade: 5");
-            } else if (num1 >= 45) {
-                System.out.println("Grade: 4");
-            }else if (num1 >= 40) {
-                System.out.println("Grade: 3");
-            }else if (num1 >= 35) {
-                System.out.println("Grade: 2");
-            }else if (num1 >= 30) {
-                System.out.println("Grade: 1");
+            if (num1 <= 150 && num1 > 0 ) {
+                System.out.println("OK");
             } else {
-                System.out.println("Failed");
+                System.out.println("Impossible");
             }
         }
     }
