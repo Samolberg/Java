ed70d73770e670d61c92214af4eced5bddf47dcd
Samolberg <l.samolberg@khk.ee>
2019-10-07 13:53:57 +0300

    Changing Variables 26 - sum of many numbers

--- print/src/changingVariables.java
+++ print/src/changingVariables.java
@@ -2,21 +2,18 @@ import java.util.Scanner;
 public class changingVariables {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        System.out.print("Enter number: ");
         int sum = 0;
-        int read;
-
-
-        System.out.print("Enter number: ");
-        read = Integer.parseInt(reader.nextLine());
-        sum = sum + read;
-        System.out.print("Enter number: ");
-        read = Integer.parseInt(reader.nextLine());
-        sum = sum + read;
-        System.out.print("Enter number: ");
-        read = Integer.parseInt(reader.nextLine());
-        sum = sum + read;
-
-        System.out.println(sum);
+        while (true) {
+            int read = Integer.parseInt(reader.nextLine());
+            if (read == 0) {
+                break;
+            }else{
+                sum = sum + read;
+                System.out.println("Sum is now: " + sum);
+            }
+        }
+        System.out.println("Sum in the end: " + sum);
 
     }
 }
