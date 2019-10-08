abfa486ea003459e11ed3e76647ef16f7fad1105
Samolberg <l.samolberg@khk.ee>
2019-10-08 12:25:05 +0300

    Changing Variables 29 2-100

--- print/src/moreLoops.java
+++ print/src/moreLoops.java
@@ -1,9 +1,14 @@
 public class moreLoops {
     public static void main(String[] args) {
-        int number = 100;
-        while (number >= 1) {
-            System.out.println(number);
-            number--;
+        int number = 1;
+        while (number <= 100) {
+
+            if (number%2 == 0){
+                System.out.println(number);
+                number++;
+            }else {
+                number++;
+            }
         }
     }
 }
