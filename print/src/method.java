7e30f8ba229def085d0aef018d6f6219e766f154
Samolberg <l.samolberg@khk.ee>
2019-10-09 11:27:01 +0300

    Method 38

--- print/src/method.java
+++ print/src/method.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
 public class method {
     public static void main(String[] args) {
-        printText();
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Times to print: ");
+        int times = Integer.parseInt(reader.nextLine());
+        int count = 0;
+
+        while (true) {
+            if (times > count) {
+                count++;
+                printText();
+            } else {
+                break;
+            }
+        }
     }
 
     public static void printText() {
