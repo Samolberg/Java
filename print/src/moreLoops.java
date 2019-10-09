50b2badb38379def268e7963ab0bdd70ec913cb7
Samolberg <l.samolberg@khk.ee>
2019-10-09 10:48:54 +0300

    Changing Variables 36.3

--- print/src/moreLoops.java
+++ print/src/moreLoops.java
@@ -4,15 +4,18 @@ public class moreLoops {
         Scanner reader = new Scanner(System.in);
         System.out.print("Enter number: ");
         int sum = 0;
+        int count = 0;
         while (true) {
 
             int number = Integer.parseInt(reader.nextLine());
             if (number == -1){
                 System.out.println("Thank you come again");
                 System.out.println("The sum is " + sum);
+                System.out.println("How many number: " + count);
                 break;
             }else {
                 sum = sum + number;
+                count++;
                 System.out.println(number);
             }
 
