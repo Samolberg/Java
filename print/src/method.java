284b3ef87c5beb91375b9306cada8ae1e23694d6
Samolberg <l.samolberg@khk.ee>
2019-10-09 11:42:53 +0300

    Method 39.1 Printing stars

--- print/src/method.java
+++ print/src/method.java
@@ -1,22 +1,21 @@
 import java.util.Scanner;
 public class method {
-    public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-        System.out.print("Times to print: ");
-        int times = Integer.parseInt(reader.nextLine());
-        int count = 0;
-
-        while (true) {
-            if (times > count) {
-                count++;
-                printText();
-            } else {
-                break;
-            }
+    public static void printStars(int amount) {
+       for(int i = 0; i<amount; i++) {
+            System.out.print("*");
         }
+
+        System.out.println("\n");
+
+
     }
 
-    public static void printText() {
-        System.out.println("In the beginning there were the swamp, the hoe and Java.\n");
+    public static void main(String[] args) {
+        printStars(5);
+        printStars(3);
+        printStars(9);
     }
+
+
+
 }
