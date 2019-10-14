b61d886f2456b8606ba4f4bca041b2c28d471843
Samolberg <l.samolberg@khk.ee>
2019-10-14 13:27:25 +0300

    Method 40.1/40.2

--- print/src/method.java
+++ print/src/method.java
@@ -4,40 +4,49 @@ public class method {
        for(int i = 0; i<amount; i++) {
             System.out.print("*");
         }
+
         System.out.print("\n");
     }
 
-    public static void main(String[] args) {
-        printTriangle(4);
-    }
+
 
     public static void printSquare(int sideSize) {
 
         for(int i = 0; i<sideSize; i++) {
             printStars(sideSize);
         }
+
         System.out.println("\n");
-    }
-
-    public static void printTriangle(int size) {
-        for(int i = size; i>=0; i--) {
-
-            printStars(size-i);
-
-
-        }
     }
 
     public static void printRectangle(int width, int height) {
 
         for(int i = 0; i<height; i++) {
             printStars(width);
-
         }
 
         System.out.println("\n");
     }
 
+    public static void printWhitespaces(int size) {
+        for(int i = 0; i<size; i++) {
+            System.out.print(" ");
+        }
+    }
 
+    public static void printTriangle(int size) {
+
+        for(int i = size; i>=0; i--){
+
+            printStars(size-i);
+            printWhitespaces(size+i);
+
+        }
+
+    }
+    public static void main(String[] args) {
+
+        printTriangle(4);
+    }
 
 }
