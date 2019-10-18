6fdd6c88452549e737c2a63da42b19a71f04942a
Samolberg <l.samolberg@khk.ee>
2019-10-18 09:57:46 +0300

    Method 43 adding numbers

--- print/src/method.java
+++ print/src/method.java
@@ -2,60 +2,16 @@ import java.util.Random;
 import java.util.Scanner;
 public class method {
 
-    static int count = 0;
-    static int numberDrawn=0;
-    private static int drawNumber() {
-            return new Random().nextInt(10);
+    public static int sum(int num1, int num2, int num3, int num4){
+
+        return (num1+num2+num3+num4);
+
     }
 
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
 
-        System.out.print("Enter number: ");
-        int guess = Integer.parseInt(reader.nextLine());
-        int numberDrawn = drawNumber();
-        int count = 0;
-
-        if (numberDrawn < guess) {
-            count++;
-            System.out.println("Number guessed is smaller. " + "Times guessed: " + count);
-            loop();
-
-        } else if (numberDrawn > guess) {
-            count++;
-            System.out.println("Number guessed is bigger. " + "Times guessed: " + count);
-            loop();
-        } else {
-            count++;
-            System.out.println("Guessed correctly! " + "Times guessed: " + count);
-
-        }
-
-    }
-
-    public static void loop() {
-
-        Scanner reader = new Scanner(System.in);
-
-        System.out.print("Enter number: ");
-        int guess = Integer.parseInt(reader.nextLine());
-        int numberDrawn = drawNumber();
-
-
-        if (numberDrawn < guess) {
-            count++;
-            System.out.println("Number is lesser. " + "Times guessed: " + count);
-            loop();
-
-        } else if (numberDrawn > guess) {
-            count++;
-            System.out.println("Number is greater. " + "Times guessed: " + count);
-            loop();
-        } else {
-            count++;
-            System.out.println("Guessed correctly! " + "Times guessed: " + count);
-
-        }
+        int answer = sum(4,3,6,1);
+        System.out.println("Total: " + answer);
     }
 
 }
