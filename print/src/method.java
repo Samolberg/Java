05fc19c76ddd3e5ba6f3e8ddad508c74355647a0
Samolberg <l.samolberg@khk.ee>
2019-10-18 10:10:40 +0300

    Method 45 greatest

--- print/src/method.java
+++ print/src/method.java
@@ -2,16 +2,22 @@ import java.util.Random;
 import java.util.Scanner;
 public class method {
 
-    public static int least (int num1, int num2){
+    public static int greatest (int num1, int num2, int num3){
 
-        return Math.min(num1, num2);
+        if (num1 >= num2 && num1 >= num3){
+            return num1;
+        }else if(num2 >= num1 && num2 >= num3){
+                return num2;
+        }else {
+            return num3;
+        }
 
     }
 
     public static void main(String[] args) {
 
-        int smaller = least(2,7);
-        System.out.println("Least: " + smaller);
+        int biggest = greatest(2,7,33);
+        System.out.println("Greatest: " + biggest);
     }
 
 }
