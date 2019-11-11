fc60927f233c0325643675bf1c24c1bb93e771b3
Samolberg <l.samolberg@khk.ee>
2019-11-11 13:30:52 +0200

    Method 51 Character separating

--- print/src/Strings.java
+++ print/src/Strings.java
@@ -8,11 +8,13 @@ public class Strings {
         Scanner reader = new Scanner(System.in);
         System.out.print("Enter name: ");
         String text = reader.nextLine();
+        int i = 0;
 
-        if(text.length() > 2) {
-            System.out.println("Character 1:" + text.charAt(0));
-            System.out.println("Character 2:" + text.charAt(1));
-            System.out.println("Character 3:" + text.charAt(2));
+        while(i < text.length()) {
+
+            System.out.println(1+i + "." + " Character " + text.charAt(i));
+            i++;
+
         }
 
     }
