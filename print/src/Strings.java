faca818c0114a8fe7103a9be0596a009a5654f29
Samolberg <larssamolberg@gmail.com>
2019-11-24 18:03:05 +0200

    Method 56 Reverse words

--- print/src/Strings.java
+++ print/src/Strings.java
@@ -3,27 +3,21 @@ import java.util.Scanner;
 public class Strings {
 
 
-    public static void main(String[] args) {
+    public static String reverse(String text) {
+        // write your code here
+        String help = "";
 
-        Scanner reader = new Scanner(System.in);
-        System.out.print("Enter string 1: ");
-        String text1 = reader.nextLine();
-
-        System.out.print("Enter string 2: ");
-        String text2 = reader.nextLine();
-
-        int index = text1.indexOf(text2);
-
-        if(index != -1) {
-            System.out.println("Common word: " + text1.substring(index));
-        }else {
-            System.out.println("Sorry they dont have match");
-
+        for (int i = text.length()-1; i >= 0; i--){
+            help += text.charAt(i);
         }
+        return help;
 
+    }
 
-
-
-
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Type in your text: ");
+        String text = reader.nextLine();
+        System.out.println("In reverse order: " + reverse(text));
     }
 }
