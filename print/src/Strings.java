35d655a9eb4152c4a0f7cf97881ed9e00c75f8df
Samolberg <larssamolberg@gmail.com>
2019-11-24 12:19:51 +0200

    Method 53 Length of a string

--- print/src/Strings.java
+++ print/src/Strings.java
@@ -6,27 +6,14 @@ public class Strings {
     public static void main(String[] args) {
 
         Scanner reader = new Scanner(System.in);
-        System.out.print("Enter name: ");
+        System.out.print("Enter string: ");
         String text = reader.nextLine();
-        int i = text.length()-1;
-        int j = 0;
+        System.out.print("Enter length: ");
+        int num1 = Integer.parseInt(reader.nextLine());
 
-        System.out.println("Revered: ");
-        while (true) {
-            if (i < 0) {
-                break;
-            }
-            System.out.print(text.charAt(i));
-            i--;
-        }
+        System.out.println("Result: " + text.substring(0,num1));
 
-    }
-    public static char calculateCharacters(String text){
-
-
-        return text.charAt(0);
 
 
     }
-
 }
