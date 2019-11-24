1b6c4f2a37442d2c58358ef0a70638cea7b5a828
Samolberg <larssamolberg@gmail.com>
2019-11-24 16:25:25 +0200

    Method 55 A word inside a word

--- print/src/Strings.java
+++ print/src/Strings.java
@@ -6,13 +6,22 @@ public class Strings {
     public static void main(String[] args) {
 
         Scanner reader = new Scanner(System.in);
-        System.out.print("Enter string: ");
-        String text = reader.nextLine();
-        System.out.print("Enter length: ");
-        int num1 = Integer.parseInt(reader.nextLine());
+        System.out.print("Enter string 1: ");
+        String text1 = reader.nextLine();
+
+        System.out.print("Enter string 2: ");
+        String text2 = reader.nextLine();
+
+        int index = text1.indexOf(text2);
+
+        if(index != -1) {
+            System.out.println("Common word: " + text1.substring(index));
+        }else {
+            System.out.println("Sorry they dont have match");
+
+        }
 
 
-        System.out.println("Result: " + text.substring(text.length()-num1,text.length()));
 
 
 
